0abf083cf27575000c913a0a1cf8b29eaa51baac
inathlia <nathtenaglia@hotmail.com>
2022-10-09 19:32:39 -0300

    teste

--- src/main/java/app/Principal.java
+++ src/main/java/app/Principal.java
@@ -12,7 +12,7 @@ public class Principal {
 		
 		AdmDAO admDAO = new AdmDAO();
 		
-		Adm adm = new Adm(-1, "amanda", "234");
+		Adm adm = new Adm(-1, "fulano", "234");
 		if(admDAO.insert(adm) == true) {
 			System.out.println("Inserção com sucesso -> " + adm.toString());
 		}
